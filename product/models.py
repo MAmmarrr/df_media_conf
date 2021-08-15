6ae5ee32752321c35d7114597a7fd095b3a79de2
Muhammad Ammar <ammarbahriang@gmail.com>
2021-08-15 02:27:40 -0700

    productDB created

--- product/models.py
+++ product/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from mongoengine import *
 # Create your models here.
-class Product(Document):
-    name = StringField(required=True)
-    price= StringField(required=True)
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    name = models.IntegerField()
